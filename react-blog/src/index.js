9cb121b0e090fad47992bd136be834e13da44248
yueyingqingshang <381265457@qq.com>
2018-02-07 23:59:35 +0800

    个人博客--后台管理首页

--- react-blog/src/index.js
+++ react-blog/src/index.js
@@ -2,13 +2,14 @@ import React,{ Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'antd/dist/antd.css';
-import Home from './pages/Home/Home'; 
+import Admin from './pages/Admin'; 
 import BlogRouter from './pages/route/router';
 import {BrowserRouter as Router,Switch,Route,Link,history} from 'react-router-dom';
 ReactDOM.render(
 	<Router>
 		<div>
 			<Switch>
+				<Route path="/admin" component={Admin}/>
 				<Route component={BlogRouter} />
 			</Switch>
 		</div>
