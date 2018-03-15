040bfa04f207f9f23fad105bd25961ccc3f3fcdd
蒋润 <381265457@qq.com>
2018-03-15 18:07:16 +0800

    2018年3月15日

--- react-blog/server/server.js
+++ react-blog/server/server.js
@@ -7,3 +7,13 @@ import connectHistoryApiFallback from 'connect-history-api-fallback';
 import config from '../config/config';
 
 const app = new Express();
+const potr = config.port;
+const targetUrl = `http://${config.apiHost}:${config.apiPort}`;
+const proxy = httpProxy.createProxyServer({
+	target: targetUrl
+});
+app.use('/api',(req,res)=>{
+	proxy.web(req,res,{target: targetUrl});
+});
+app.use('/',Express.static(path.join(__dirname,"",'build')));
+app.use('/',connectHistoryApiFallback());
