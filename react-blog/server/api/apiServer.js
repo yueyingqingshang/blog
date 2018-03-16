a2589da1b3fee541e05bc2ec35d2a11e2ae46623
蒋润 <381265457@qq.com>
2018-03-16 18:34:02 +0800

    3月16日--apiServer

--- react-blog/server/api/apiServer.js
+++ react-blog/server/api/apiServer.js
@@ -9,6 +9,22 @@ const port = config.apiPort;
 const app = new Express();
 
 //控制路由
-app.use('/',require('./main'));
+//app.use('/',require('./main'));
+//管理页面路由
+//app.use('/admin',require('./admin'));
 //链接数据库
-mongoose.Promist
+mongoose.Promist = require('bluebird');
+mongoose.conect(`mongodb://${config.dbHost}:${config.dbPort}/myblog`,function(err) {
+	if(err) {
+		console.log('数据库链接失败');
+		return;
+	}
+	console.log('数据库链接成功');
+	app.listen(port,function(err) {
+		if (err) {
+            console.error('err:', err);
+        } else {
+            console.info(`===> api server is running at ${config.apiHost}:${config.apiPort}`)
+        };
+	});
+});
